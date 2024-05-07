ad4a45e0d08b30f434d1b0e9370e0b848d55fce1
Kam <sirkamronwilson00@gmail.com>
2024-05-07 08:09:16 -0500

    added shadcn and fixed some tailwind config

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,12 +3,47 @@ import DashboardSearch from "./DashboardSearch"
 
 type DashboardCardProps = {
 	title: string
-	description: string
+	date: string
 }
+
+const taskCards = [
+	{
+		title: "Design Marketing Campaign",
+		date: "25 Apr 2024",
+	},
+	{
+		title: "Create Presentation Deck",
+		date: "24 Apr 2024",
+	},
+	{
+		title: "UI/UX Designs for Website",
+		date: "23 Apr 2024",
+	},
+	{
+		title: "Develop Feature X",
+		date: "22 Apr 2024",
+	},
+	{
+		title: "Finish Project Documentation",
+		date: "21 Apr 2024",
+	},
+	{
+		title: "Review Codebase",
+		date: "20 Apr 2024",
+	},
+	{
+		title: "Do Homework",
+		date: "19 Apr 2024",
+	},
+	{
+		title: "Conduct Team Meeting",
+		date: "18 Apr 2024",
+	},
+]
 
 export default function Dashboard() {
 	return (
-		<div className='flex flex-col bg-neutral-900 p-5 h-full'>
+		<div className='flex flex-col bg-neutral-900 p-5 h-full overflow-y-auto'>
 			<div className='mb-4'>
 				<DashboardSearch />
 			</div>
@@ -18,21 +53,33 @@ export default function Dashboard() {
 						CREATE
 					</button>
 				</div>
-				<div className='flex flex-row space'></div>
+				<div className='flex flex-col space'>
+					<div className='mb-4'>
+						<p className='text-neutral-100 text-4xl font-bold'>Tasks</p>
+					</div>
+					<div className='flex flex-col w-1/2'>
+						<div className='mb-4'>
+							{taskCards.map((task, index) => (
+								<div key={index} className='mb-4'>
+									<DashboardCard title={task.title} date={task.date} />
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
 }
 
-export function DashboardCard(props: DashboardCardProps) {
-	const { title, description } = props
+export const DashboardCard: React.FC<DashboardCardProps> = (
+	props: DashboardCardProps
+) => {
+	const { title, date } = props
 	return (
-		<div className='bg-neutral-800 rounded h-[300px] w-[300px] p-2'>
-			<div className='h-1/6'>
-				<p className='text-neutral-50 text-2xl font-bold'>{title}</p>
-			</div>
-			<p className='text-neutral-500'>{description}</p>
-			<div></div>
+		<div className='bg-neutral-800 h-[150px] rounded-xl p-2'>
+			<p className='text-2xl text-neutral-100'>{title}</p>
+			<p className='text-neutral-500'>{date}</p>
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+	darkMode: ["class"],
+	content: [
+		"./index.html",
+		"./src/pages/**/*.{ts,tsx}",
+		"./src/components/**/*.{ts,tsx}",
+		"./src/app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
+		"./@/**/*.{ts, tsx}",
+	],
+	// prefix: "tailwind.config.js",
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
 			screens: {
 				sm: "480px",
 				md: "768px",
@@ -54,17 +84,79 @@ export default {
 				sans: ["Graphik", "sans-serif"],
 				serif: ["Merriweather", "serif"],
 			},
-			extend: {
-				spacing: {
-					128: "32rem",
-					144: "36rem",
-				},
-				borderRadius: {
-					"4xl": "2rem",
-				},
-			},
 		},
 	},
-
-	plugins: [],
+	plugins: [require("tailwindcss-animate")],
 }
+
+// OLD CONFIG
+// /** @type {import('tailwindcss').Config} */
+// export default {
+// 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// 	theme: {
+// 		extend: {
+// 			screens: {
+// 				sm: "480px",
+// 				md: "768px",
+// 				lg: "976px",
+// 				xl: "1440px",
+// 			},
+// 			colors: {
+// 				neutral: {
+// 					// Gray
+// 					50: "#f9fafb",
+// 					100: "#f3f4f6",
+// 					200: "#e5e7eb",
+// 					300: "#d1d5db",
+// 					400: "#9ca3af",
+// 					500: "#6b7280",
+// 					600: "#4b5563",
+// 					700: "#374151",
+// 					800: "#1f2937",
+// 					900: "#111827",
+// 				},
+// 				primary: {
+// 					// Indigo
+// 					50: "#eef2ff",
+// 					100: "#e0e7ff",
+// 					200: "#c7d2fe",
+// 					300: "#a5b4fc",
+// 					400: "#818cf8",
+// 					500: "#6366f1",
+// 					600: "#4f46e5",
+// 					700: "#4338ca",
+// 					800: "#3730a3",
+// 					900: "#312e81",
+// 				},
+// 				secondary: {
+// 					// Rose
+// 					50: "#fff1f2",
+// 					100: "#ffe4e6",
+// 					200: "#fecdd3",
+// 					300: "#fda4af",
+// 					400: "#fb7185",
+// 					500: "#f43f5e",
+// 					600: "#e11d48",
+// 					700: "#be123c",
+// 					800: "#9f1239",
+// 					900: "#881337",
+// 				},
+// 			},
+// 			fontFamily: {
+// 				sans: ["Graphik", "sans-serif"],
+// 				serif: ["Merriweather", "serif"],
+// 			},
+// 			extend: {
+// 				spacing: {
+// 					128: "32rem",
+// 					144: "36rem",
+// 				},
+// 				borderRadius: {
+// 					"4xl": "2rem",
+// 				},
+// 			},
+// 		},
+// 	},
+
+// 	plugins: [],
+// }
